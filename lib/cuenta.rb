a30a6d0d22a669722637e8dfa39b36f63c12ef99
victor <victorhm2011@gmail.com>
2020-07-01 17:14:58 -0400

    primera prueba pasando

--- lib/cuenta.rb
+++ lib/cuenta.rb
@@ -2,4 +2,8 @@ class Cuenta
     def initialize()
         @balance=0
     end
+
+    def getBalance()
+        return 0
+    end
 end
--- spec/cuenta_spec.rb
+++ spec/cuenta_spec.rb
@@ -1,4 +1,6 @@
-Rspec.describe "Creacion cuenta" do
+require './lib/cuenta.rb'
+
+RSpec.describe "Creacion cuenta" do
     it 'deberia devolverme el balance inicial 0' do
         @cuenta=Cuenta.new
         expect(@cuenta.getBalance()).to eq(0)
