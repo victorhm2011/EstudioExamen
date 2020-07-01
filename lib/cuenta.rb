8cbbcbbb088bc38a13e8374e34f97ed55d37a95b
victor <victorhm2011@gmail.com>
2020-07-01 17:10:03 -0400

    cracion clase cuenta

---
+++ lib/cuenta.rb
@@ -0,0 +1,5 @@
+class Cuenta
+    def initialize()
+        @balance=0
+    end
+end
---
+++ spec/cuenta_spec.rb
@@ -0,0 +1,6 @@
+Rspec.describe "Creacion cuenta" do
+    it 'deberia devolverme el balance inicial 0' do
+        @cuenta=Cuenta.new
+        expect(@cuenta.getBalance()).to eq(0)
+    end
+end
